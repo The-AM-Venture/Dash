4557810750f16deada308c4a36e350f56ee9fe62
Alfred <deshawn.malone14@gmail.com>
2025-10-17 17:46:51 -0500

    add chart component

---
+++ src/app/components/chart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Chart() {
+  return (
+    <>
+        <div className='w-lg h-[20rem] bg-sky-500'>
+            asdfds
+        </div>
+    </>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Sidebar from "./components/sidebar";
 import Nav from "./components/nav";
 import Trending from "./components/trending";
+import Chart from "./components/chart";
 
 export default function Home() {
   return (
@@ -9,8 +10,12 @@ export default function Home() {
       <section className="flex flex-row">
         <Sidebar />
         <div className="flex flex-col w-full h-full">
-        <Nav />
-        <Trending />
+          <Nav />
+          <Trending />
+          <div className="p-5">
+            <Chart />
+
+          </div>
         </div>
       </section>
     </>
