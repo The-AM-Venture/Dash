5265118faea413b4f3ee4fe8261820a0bbe9111b
Alfred <deshawn.malone14@gmail.com>
2025-10-26 13:30:55 -0500

    fix overflow issue

--- src/app/components/trending.js
+++ src/app/components/trending.js
@@ -13,12 +13,10 @@ const testtrends = [
 export default function Trending() {
   return (
     <>
-        <div className=''>
-        <div className='flex flex-row flex-nowrap overflow-x-auto'>
+        <div className='flex flex-row  absolute '>
             {
                 testtrends.map( (link,i) => 
-                    <div key={i} className='border-3 bg-surface-a0 text-light-a0  border-b-6 border-surface-a10 grow h-20 p-2'>
-                        <div className='h-full'>
+                    <div key={i} className='border-3 bg-surface-a0 text-light-a0 md:w-80 w-40 border-b-3 border-surface-a10 p-2'>
                         <div className='flex md:flex-row flex-col justify-between lg:text-justify text-center lg:text-lg text-xs'>
                             <p>BTX</p>
                             <div className='flex flex-col'>
@@ -29,14 +27,10 @@ export default function Trending() {
                             </div>
                             
                         </div>
-
-                        </div>
                     </div>
                 )
             }
 
-
-        </div>
 
         </div>
 
--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,13 @@ import News from "./components/news";
 export default function Home() {
   return (
     <>
-      <section className="flex flex-row w-auto">
+      <section className="flex flex-row">
         <Sidebar />
         <div className="flex flex-col">
           <Nav />
+          <div className="relative w-full overflow-x-auto h-20">
           <Trending />
+          </div>
           <div className=" flex 2xl:flex-row xl:flex-col lg:flex-col md:flex-col flex-col p-5">
             <Chart />
             <News />
